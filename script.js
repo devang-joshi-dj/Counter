767719faeefa1b2f5133cf7b63a9a0c198f52330
Devang Joshi <devangjoshi11@gmail.com>
2021-02-19 20:45:20 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 "use strict";
 
+// variables declaration to be used in the code
 const counter = document.querySelector('#counter');
 const upBtn = document.querySelector('#up');
 const downBtn = document.querySelector('#down');
 const resetBtn = document.querySelector('#reset');
 
 window.onload = () => {
+    // functions to be executed when page is loaded
+
     counter.innerHTML = 0;
     downBtn.addEventListener('click', downBtnAction);
     upBtn.addEventListener('click', upBtnAction);
@@ -13,20 +16,22 @@ window.onload = () => {
 }
 
 const downBtnAction = () => {
+    // function responsible for manipulating the value of counter to the negative side
     counter.innerHTML--;
 }
 
 const upBtnAction = () => {
+    // function responsible for manipulating the value of counter to the positive side
     counter.innerHTML++;
 }
 
 const resetBtnAction = () => {
+    // function responsible for manipulating the value of counter by resetting it
     counter.innerHTML = 0;
 }
 
 document.addEventListener('keyup', (event) => {
     // Events which will happen if a specific key is pressed Up
-    // Events refers to highlight of specific key when that specific key is pressed on keyboard
     const e = event.which;
     switch (e) {
         case 32:
